c66d0f81000a47e8cc5e7d9e68596469de2a2ae2
unknown <rm@vintage.com.ua>
2021-10-24 09:17:06 +0300

    before merging

--- pages/_id.vue
+++ pages/_id.vue
@@ -6,10 +6,6 @@ d/g,d'fg
 
 <script>
   export default {
-    async created () {
-      const db = await this.$fireStore
-      console.log('dbdddd', db, this.$route.params.id);
-    },
     async asyncData(config, { params, redirect }) {
       console.log('dbd111111111111ddd', params, config);
     }
